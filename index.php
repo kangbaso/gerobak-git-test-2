26ed9516b90aa137b9195218c3c82ae0206910af
Lucky Junihardi <bulepian@gmail.com>
2020-01-06 11:16:28 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello Worrld!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum similique ullam modi nobis, a mollitia commodi laboriosam nemo esse qui minus exercitationem deleniti, eligendi facilis aut? Expedita aperiam eveniet iure?</P>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
